1f0c8725ab4532b1f7c792874f9168ca72b940d0
alex-krest <73220512+alex-krest@users.noreply.github.com>
2022-02-03 21:29:26 +0200

    Сделано

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,37 +3,33 @@ const refs={
 	formEl: document.querySelector(".feedback-form"),
 	emailEl: document.querySelector(".feedback-form input"),
 	messageEl: document.querySelector(".feedback-form textarea"),
-	submitEl: document.querySelector(".feedback-form button"),
 }
 const LOCALSTORAGE_KEY="feedback-form-state";
 
 const formData={};
 
+
+const checkingLocalstorage = function () {
+const formData=JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+if (formData===null) 
+	{return}
+	refs.emailEl.value = formData.email, 
+	refs.messageEl.value =formData.message;
+}
+checkingLocalstorage();
 // ---------------------------------------------------------
 refs.formEl.addEventListener("input", throttle((onFormInput),500));
 refs.formEl.addEventListener("submit", onFormSubmit)
 // --------------------------------------------------------
 function onFormInput (e){
 	formData[e.target.name]= e.target.value;
-	console.log(formData);
 		localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
 };
 
-const keyValueStart = function () {
-	const dataJson=localStorage.getItem(LOCALSTORAGE_KEY);
-	formData=JSON.parse(dataJson);
-if (dataJson) 
-	{
-	// refs.emailEl.value===keyValue.email.value;
-	// refs.messageEl.value===keyValue.message.value;
-	}}
-
 function onFormSubmit (e)  {
 	e.preventDefault();
-	const {
-	  elements: { email, message }
-	} = e.currentTarget;
-	console.log(email.value, message.value);
+	console.log(formData);
 e.currentTarget.reset();
 localStorage.removeItem(LOCALSTORAGE_KEY);
  };
+
