e38b439fc87e168448eb502f3a9baa7ee22dd2b7
alex-krest <73220512+alex-krest@users.noreply.github.com>
2022-02-04 22:22:46 +0200

    Исправил отправку формы

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,35 +1,38 @@
-import throttle from "lodash.throttle";
-const refs={
-	formEl: document.querySelector(".feedback-form"),
-	emailEl: document.querySelector(".feedback-form input"),
-	messageEl: document.querySelector(".feedback-form textarea"),
-}
-const LOCALSTORAGE_KEY="feedback-form-state";
+import throttle from 'lodash.throttle';
+const refs = {
+  formEl: document.querySelector('.feedback-form'),
+  emailEl: document.querySelector('.feedback-form input'),
+  messageEl: document.querySelector('.feedback-form textarea'),
+  submitEl: document.querySelector('.feedback-form button'),
+};
+console.log(refs.submitEl);
+const LOCALSTORAGE_KEY = 'feedback-form-state';
 
-const formData={};
-
+const formData = {};
 
 const checkingLocalstorage = function () {
-const formData=JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-if (formData===null) 
-	{return}
-	refs.emailEl.value = formData.email, 
-	refs.messageEl.value =formData.message;
-}
+  const formData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  if (formData === null) {
+    return;
+  }
+  (refs.emailEl.value = formData.email), (refs.messageEl.value = formData.message);
+};
 checkingLocalstorage();
 // ---------------------------------------------------------
-refs.formEl.addEventListener("input", throttle((onFormInput),500));
-refs.formEl.addEventListener("submit", onFormSubmit)
+refs.formEl.addEventListener('input', throttle(onFormInput, 500));
+refs.formEl.addEventListener('submit', onFormSubmit);
 // --------------------------------------------------------
-function onFormInput (e){
-	formData[e.target.name]= e.target.value;
-		localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
-};
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
 
-function onFormSubmit (e)  {
-	e.preventDefault();
-	console.log(formData);
-e.currentTarget.reset();
-localStorage.removeItem(LOCALSTORAGE_KEY);
- };
-
+function onFormSubmit(e) {
+  e.preventDefault();
+  if (refs.emailEl.value === '' || refs.messageEl.value === '') {
+    return;
+  }
+  console.log(formData);
+  e.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
